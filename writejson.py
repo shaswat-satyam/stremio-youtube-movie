f73fbd69f5d4856161d28cef6e67a792f7afb994
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2025-03-18 11:17:57 +0530

    Python script to convert title and youtube url to js object

---
+++ writejson.py
@@ -0,0 +1,26 @@
+import json
+import urllib.request
+output = open("data2.json","a")
+omdbKEY = "ce74ad75"
+
+try:
+    while True:
+        name = input("Enter the name of the Movie or (q)uit:")
+        if name == "q":
+            break
+        youtubeURL = input("Enter the youtube URL: ")
+        url = "http://www.omdbapi.com/?t="+"+".join(name.split())+"&apikey="+omdbKEY
+        print(url)
+        response = urllib.request.urlopen(url)
+        result = json.loads(response.read().decode('utf-8'))
+        if result.get("Response") == "False":
+            continue
+        print(result)
+        finalResult = {result["imdbID"]:{}}
+        finalResult[result["imdbID"]]["name"] = result["Title"]
+        finalResult[result["imdbID"]]["type"] = "movie"
+        finalResult[result["imdbID"]]["ytId"] = youtubeURL.split("=")[-1]
+        output.write(json.dumps(finalResult))
+        print(f"Written {result['Title']} to the file")
+finally:
+    output.close()
